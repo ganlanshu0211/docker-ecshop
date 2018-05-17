d72b54556a920b41cb22eb81334670aeefe236c1
gls <260096556@qq.com>
2018-05-17 10:21:07 +0800

    Signed-off-by: gls <260096556@qq.com>

--- nginx-php/start.sh
+++ nginx-php/start.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 docker build -t ecshop .
-docker run --name ecshop -p 80:80 -v /Users/wilf/Documents/lumen/source/:/var/www/html --link mysql:mysql -d ecshop
+docker run --name ecshop -p 80:80 -v /data/tool/:/var/www/html --link mysql:mysql -d ecshop
--- source/ecshop/h5/config/config.app.js
+++
@@ -1,9 +0,0 @@
-var GLOBAL_CONFIG = {
-	'API_HOST': 'http://api.0.0.0.0',
-	'APP_NAME': 'Demo',
-	'APP_DESC': 'This is a demo',
-	'APP_KEYWORDS': 'Demo, Shop',
-	'DEBUG': false,
-	'ENCRYPTED': false,
-	'FOR_WEIXIN': false
-};
